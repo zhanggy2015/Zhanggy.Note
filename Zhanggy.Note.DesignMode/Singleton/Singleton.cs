a3d69c98f7d6457e8a79f97adc89807be6fd5180
zhanggy2015 <445157447@qq.com>
2019-03-16 17:50:03 +0800

    单例模式三种写法

---
+++ Zhanggy.Note.DesignMode/Singleton/Singleton.cs
@@ -0,0 +1,58 @@
+﻿/// <summary>
+/// 单例模式：1 构造函数私有化 2.永远只有一个实例化对象,并提供一个全局访问点
+/// </summary>
+namespace Zhanggy.Note.DesignMode.Singleton
+{
+    /// <summary>
+    /// 静态构造函数方式
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton intance;
+        private Singleton() { }
+
+        static Singleton() {
+            //由CLR保证只实例化一次
+            intance = new Singleton();
+        }
+
+        public static Singleton Intance() {
+            return intance;
+        }
+    }
+
+    /// <summary>
+    /// 静态对象方式
+    /// </summary>
+    public class SingletonSecond {
+
+        //由CLR保证只实例化一次
+        private static SingletonSecond intance = new SingletonSecond();
+        private SingletonSecond() { }
+
+        public static SingletonSecond Intance() {
+            return intance;
+        }
+    }
+
+    /// <summary>
+    /// 双判断加锁
+    /// </summary>
+    public class SingletonThird {
+
+        private readonly static object objLock=new object();
+        private static SingletonThird intance;
+        private SingletonThird() { }
+
+        public static SingletonThird Intance() {
+            if (intance==null)
+            {
+                lock (objLock) {
+                    if (intance == null)
+                        intance = new SingletonThird();
+                }
+            }
+            return intance;
+        }
+    }
+}
