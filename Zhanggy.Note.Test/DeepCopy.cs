bf6df6f443e813d4c7ae3836f3cf8c125a212440
zhanggy2015 <445157447@qq.com>
2019-04-27 22:57:28 +0800

    浅复制和深复制

---
+++ Zhanggy.Note.Test/DeepCopy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Zhanggy.Note.Test
+{
+    public class DeepCopy : ICloneable
+    {
+        public int _i = 0;
+        public A _a = new A();
+
+        /// <summary>
+        /// 实现深复制
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            DeepCopy newObj = new DeepCopy();
+            newObj._i = _i;
+            newObj._a = new A();
+            newObj._a._s = _a._s;
+            return newObj;
+        }
+
+        /// <summary>
+        /// 实现浅复制
+        /// </summary>
+        /// <returns></returns>
+        public new object MemberwiseClone()
+        {
+            return base.MemberwiseClone();
+        }
+    }
+
+    public class A
+    {
+        public string _s = "copy test!";
+    }
+}
