cd0b5c86e16888e5b1d2fe0fa0fd076e2dfb1f15
zhanggy2015 <zhanggy2019@gmail.com>
2019-07-25 22:00:58 +0800

    Js常用函数

---
+++ Zhanggy.Note/Js/Common.js
@@ -0,0 +1,28 @@
+﻿
+//移入移除
+var initTip = function (dataText, selector) {
+    $(selector).focus(function () {
+        $(this).removeClass("gray");
+        if ($(this).val() == dataText) {
+            $(this).val("");
+        }
+    }).blur(function () {
+        if ($(this).val() == "") {
+            $(this).val(dataText);
+        }
+        if ($(this).val() == dataText) {
+            $(this).addClass("gray");
+        } else {
+            $(this).removeClass("gray");
+        }
+    });
+}
+
+//延迟n秒执行某函数
+var delay = function (fn, n) {
+    var openTimer = null;
+    openTimer = setTimeout(function () {
+        clearTimeout(openTimer);
+        fn();
+    }, n);
+}
